c0bc6401a34c173bd578414948dcf446ba9772f3
OmegaJak <jak.kruger@gmail.com>
2014-10-13 15:56:30 -0500

    Update communication.js

--- communication.js
+++ communication.js
@@ -1,9 +1,10 @@
-
-
 var accountSid = ''; // Fill this in when account is made.
-var authToken = "{{ auth_token }}"; //not sure what this is, do something here
+var authToken = "{{ auth_token }}"; // Not sure what this is, do something here
 var client = require('twilio')(accountSid, authToken);
- 
+
+var app = require('express');
+app.POST('/receive',function() {});
+
 client.messages(message.sid).get(function(err, message) {
     var body = message.body;
     var number_regex = /[1234567890]{1,50}/;
