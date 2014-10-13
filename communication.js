95058bd24b01610012e0d6f400435e2bfe57624b
Episkeyy <sky06286@gmail.com>
2014-10-13 17:34:28 -0500

    Update communication.js
    Added some preliminary conditionals and clarifications and things. If they're unnecessary just remove them

--- communication.js
+++ communication.js
@@ -1,12 +1,17 @@
 var accountSid = ''; // Fill this in when account is made.
-var authToken = "{{ auth_token }}"; // Not sure what this is, do something here
+var authToken = "{{ auth_token }}"; // Not sure what this is, do something here.
 var client = require('twilio')(accountSid, authToken);
 
-var app = require('express');
+var app = require('express'); // Move this if necessary.
 app.POST('/receive',function() {});
 
 client.messages(message.sid).get(function(err, message) {
     var body = message.body;
-    var number_regex = /[1234567890]{1,50}/;
-    var numbers = body.match(number_regex);
+    var number_regex = /[1234567890]{1,50}/; // Change the latter number depending on the math constraints.
+    var numbers = body.match(number_regex); // match should return an array with the matches in them, if any.
+    if (numbers.length < 2) {
+        // React accordingly here
+    } else if (numbers.length > 2) {
+        // React accordingly here
+    } else {} // Do we need something here?
 });
