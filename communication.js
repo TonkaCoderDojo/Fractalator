d7a7c58bbd89e34c3b5beabaa9ce63db06fd716c
Episkeyy <sky06286@gmail.com>
2014-10-14 15:29:31 -0500

    Added some conditional stuff.

--- communication.js
+++ communication.js
@@ -6,12 +6,24 @@ var app = require('express'); // Move this if necessary.
 app.POST('/receive',function() {});
 
 client.messages(message.sid).get(function(err, message) {
-    var body = message.body;
     var number_regex = /[1234567890]{1,50}/; // Change the latter number depending on the math constraints.
-    var numbers = body.match(number_regex); // match should return an array with the matches in them, if any.
+    var numbers = message.body.match(number_regex); // match should return an array with the matches in them, if any.
     if (numbers.length < 2) {
-        // React accordingly here
+        client.messages.create({
+            body: "You did not send enough numbers. Please try again.",
+            to: message.from, // Not sure if this is correct, and the documentation doesn't seem to help.
+            from: "" // Put number here.
+        }, function(err, message) {
+            process.stdout.write(message.sid);
     } else if (numbers.length > 2) {
-        // React accordingly here
-    } else {} // Do we need something here?
+        client.messages.create({
+        /* Right now, I'm assuming that we only want two numbers.
+        Alternatively, we could just take the first two that they send in.
+        If we want to do that, just change it. */
+            body: "You sent too many numbers. Please try again.",
+            to: message.from, // Again, unsure if this is correct.
+            from: "" // Put number here.
+        }, function(err, message) {
+            process.stdout.write(message.sid);
+    } else {} // Do something here.
 });
